1aa3d5fbccb9b430c9ab92d868b6ba9a5bed7fbe
Connor Adams <cnnr252@gmail.com>
2018-09-01 10:36:10 -0400

    Excluded weekends

--- parse-weather.js
+++ parse-weather.js
@@ -2,19 +2,28 @@ const fs = require('fs');
 var csv = require('csv-parser')
 const snowData = JSON.parse(fs.readFileSync("./data.json").toString());
 const allData = [];
+let i = 0;
 fs.createReadStream('./weather.csv')
     .pipe(csv())
     .on('data', function (data) {
-        for (snowday of snowData) {
-            if (snowday.time.includes(data['Date/Time'])) {
-                data["snowday"] = true;
-                console.log('snow!');
-                allData.push(data);
-                return;
+        if (i != 0 && i != 6) {
+            for (snowday of snowData) {
+                if (snowday.time.includes(data['Date/Time'])) {
+                    data["snowday"] = true;
+                    console.log('snow!');
+                    allData.push(data);
+                    if (i == 6)
+                        i = -1;
+                    i++;
+                    return;
+                }
             }
+            data["snowday"] = false;
+            allData.push(data);
+        } else if (i == 6) {
+            i = -1;
         }
-        data["snowday"] = false;
-        allData.push(data);
+        i++;
     }).on('end', function () {
         console.log(allData);
         fs.writeFile("./compiled.json", JSON.stringify(allData, null, 2), function (err) {
